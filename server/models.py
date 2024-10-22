9b6016307d1427ef80dac3a009b0558c4ff6766d
Alin Silcock <at.silcock@gmail.com>
2024-10-22 14:32:10 -0600

    updating models

--- server/models.py
+++ server/models.py
@@ -5,4 +5,47 @@ from config import db
 
 # Models go here!
 
-pass
+class State(db.Model, SerializerMixin):
+    __tablename__ = "states"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    date_moved_to_state = db.Column(db.Date) 
+
+    license_info_id = db.Column(db.Integer, db.ForeignKey('licenseInfo.id'))
+
+
+
+class Car(db.Model, SerializerMixin):
+    __tablename__ = "cars"
+
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String, nullable=False)
+    model = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    color = db.Column(db.String, nullable=False)  
+    vin = db.Column(db.String)
+
+
+   
+
+class LicenseInfo(db.Model, SerializerMixin):
+    __tablename__ = "licenseInfo"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    license_number = db.Column(db.String, nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    state_id = db.Column(db.Integer, db.ForeignKey('states.id'))
+    address = db.Column(db.String, nullable=False)
+
+
+
+
+
+
+
+    
+
+
+
