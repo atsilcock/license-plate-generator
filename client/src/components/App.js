636d01fd1861f5dc0032220c99f7ea9143360848
Alin Silcock <at.silcock@gmail.com>
2024-10-26 08:16:08 -0600

    updating to useContext

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,44 +1,48 @@
-import React, { useState, useEffect } from "react"
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from "./Home"
-import NavBar from "./NavBar"
-import DisplayDriver from "./DisplayDriver"
-import UpdateDriver from "./UpdateDriver"
-import ViewStateInfo from "./ViewStateInfo"
-import NewCar from "./NewCar"
+import React, { createContext, useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./Home";
+import NavBar from "./NavBar";
+import DisplayDriver from "./DisplayDriver";
+import UpdateDriver from "./UpdateDriver";
+import ViewStateInfo from "./ViewStateInfo";
+import NewCar from "./NewCar";
+
+// Create contexts
+export const DriversContext = createContext();
+export const LicenseInfoContext = createContext();
 
 function App() {
-  const [drivers, setDrivers] = useState([])
-  console.log(drivers)
-
-  const[licenseInfo, setLicenseInfo] = useState([])
-  console.log(licenseInfo)
-
+  const [drivers, setDrivers] = useState([]);
+  const [licenseInfo, setLicenseInfo] = useState([]);
 
   useEffect(() => {
     fetch("/drivers")
       .then((response) => response.json())
-      .then((data) => setDrivers(data))
-  }, [])
+      .then((data) => setDrivers(data));
+  }, []);
 
   useEffect(() => {
     fetch("/licenseinfo")
-    .then((response) => response.json())
-    .then((data) => setLicenseInfo(data))
-  }, [])
+      .then((response) => response.json())
+      .then((data) => setLicenseInfo(data));
+  }, []);
 
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/display-car-info" component={() => <DisplayDriver drivers= {drivers} licenseInfo= {licenseInfo} setDrivers={setDrivers} />} />
-        <Route path="/update-car-info" component={() => <UpdateDriver driver={drivers} setDrivers={setDrivers} />} />
-        <Route path="/view-state-info" component={() => <ViewStateInfo />} />
-        <Route path="/register-car" component={() => <NewCar driver={drivers} setDrivers={setDrivers}/>} />
-      </Switch>
-    </Router>
+    <DriversContext.Provider value={{ drivers, setDrivers }}>
+      <LicenseInfoContext.Provider value={{ licenseInfo, setLicenseInfo }}>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/display-car-info" component={DisplayDriver} />
+            <Route path="/update-car-info" component={UpdateDriver} />
+            <Route path="/view-state-info" component={ViewStateInfo} />
+            <Route path="/register-car" component={NewCar} />
+          </Switch>
+        </Router>
+      </LicenseInfoContext.Provider>
+    </DriversContext.Provider>
   );
 }
 
-export default App
+export default App;
